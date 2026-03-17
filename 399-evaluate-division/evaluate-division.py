56dfd41cf315c5f33608f27b391c0e2238947dbf
Arindam0231 <80635074+Arindam0231@users.noreply.github.com>
2026-03-17 23:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (91.11%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,44 +1,40 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def dfs(graph, src, dst,visited, cost_overall):
-            print(src, dst, cost_overall)
-            if cost_overall == -1:
-                return -1
-            if src==dst:
-                return cost_overall
-            if src not in graph:
-                return -1
-            
-            for to, cost in graph[src]:
-                if to not in visited:
-                    visited.add(to)
-                    t = dfs(graph, to,dst, visited,cost*cost_overall) 
-                    if t!=-1:
-                        return t
-                    visited.remove(to)
-            return -1
-        from collections import defaultdict
-        nodes = set()
-        head =set()
-        graph = defaultdict(list)
-        for i in range(len(equations)):
-            fro, to = equations[i]
-            nodes.add(fro)
-            nodes.add(to)
-            head.add(fro)
-            graph[fro].append((to, values[i])) 
-            graph[to].append((fro, 1/values[i])) 
-        result = []
-        for fro, to in queries:
-            if fro not in graph or to not in nodes:
-                result.append(-1)
-                continue
-            if fro == to:
-                result.append(1)
-                continue
-            cost = dfs(graph, fro,to,set([fro]),1)
-            result.append(cost)
+        parent = {}
+        weight = {}  # weight[x] = x / parent[x]
 
+        def find(x):
+            if parent[x] != x:
+                root = find(parent[x])
+                weight[x] *= weight[parent[x]]
+                parent[x] = root
+            return parent[x]
+
+        def union(a, b, val):
+            ra, rb = find(a), find(b)
+            if ra == rb:
+                return
+            # ra/rb = val * weight[b] / weight[a]
+            weight[ra] = val * weight[b] / weight[a]
+            parent[ra] = rb
+
+        for (a, b), val in zip(equations, values):
+            if a not in parent:
+                parent[a] = a
+                weight[a] = 1.0
+            if b not in parent:
+                parent[b] = b
+                weight[b] = 1.0
+            union(a, b, val)
+
+        result = []
+        for a, b in queries:
+            if a not in parent or b not in parent:
+                result.append(-1.0)
+            elif find(a) != find(b):
+                result.append(-1.0)
+            else:
+                result.append(weight[a] / weight[b])
         return result
 
         
